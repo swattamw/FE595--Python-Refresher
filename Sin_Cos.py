c5dcd6ec8f438d17d383e0d95d2f51b05e04e750
Shreyas Wattamwar <shreyasitd@gmail.com>
2019-02-13 09:38:46 -0500

    Tangent

--- Sin_Cos.py
+++ Sin_Cos.py
@@ -6,10 +6,11 @@ def sin_cos_curve(period):
     time = np.arange(0, period * 2* π, 0.1)
     amplitud_sin = np.sin(time)
     amplitud_cos = np.cos(time)
-
+    amplitud_tan = np.tan(time)
     fig, plot = plt.subplots()
     plt.plot(time, amplitud_sin, label = 'Sine')
     plt.plot(time, amplitud_cos, label = 'Cosine')
+    plt.plot(time,amplitud_tan, label= 'Tangent')
     plt.axhline(y = 0, color='black')
     
     xticks = np.arange(0, (period*2*π)+(π/2), π/2)
@@ -21,8 +22,9 @@ def sin_cos_curve(period):
     plot.set_xlim((0, period * 2 * np.pi)) 
     plot.set_ylim((-1, 1))
     
-    plt.title(str(period)+'-Period: '+'Sin-Cos', fontsize=20)
+    plt.title(str(period)+'-Period: '+'Sin-Cos-tan', fontsize=20)
     plt.legend()
     plt.show()
-    
+    fig.savefig('/Users/shreyastro/Desktop/Desktop_files/Stevens_Statements/spring_2019/FE595/Assignment1/Tan.png')
+
 sin_cos_curve(period = 1)
